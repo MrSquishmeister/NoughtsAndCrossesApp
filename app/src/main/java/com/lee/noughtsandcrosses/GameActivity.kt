5fb33e4ec76286b200b4e5eb7f6dc109730347b8
Ainsley <120529093+MrSquishmeister@users.noreply.github.com>
2023-01-08 15:20:25 +0000

    switch activity feat : button click between main and game activity

--- app/src/main/java/com/lee/noughtsandcrosses/GameActivity.kt
+++ app/src/main/java/com/lee/noughtsandcrosses/GameActivity.kt
@@ -5,16 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_game)
 
-        val button: Button = findViewById(R.id.startButton)
+        val button: Button = findViewById(R.id.mainButton)
         button.setOnClickListener {
-            val intent = Intent(this, GameActivity::class.java)
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
-
     }
 }
